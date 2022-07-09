c83ad9a9cc34203cac30ca17275c47b76c73fd51
rajesh948 <103615270+rajesh948@users.noreply.github.com>
2022-07-09 07:56:46 +0530

    Add files via upload

---
+++ BookStore.Models/Models/GetCartModel.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Models
+{
+    public class GetCartModel
+    {
+        public GetCartModel(int Id, int Userid, BookModel Book, int Quantity)
+        {
+            this.Id = Id;
+            this.Userid = Userid;
+            this.Book = Book;
+            this.Quantity = Quantity;
+        }
+        public int Id { get; set; }
+        public int Userid { get; set; }
+        public BookModel Book { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ BookStore.Models/ViewModels/Cart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BookStore.Models.ViewModels
+{
+    public partial class Cart
+    {
+    public Cart()
+        {
+               Books = new HashSet<Book>();
+             Users = new HashSet<User>();
+        }
+
+        public int Id { get; set; }
+        public int Userid { get; set; }
+       public int Bookid { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual Book Book { get; set; }
+        public virtual User User { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+        public virtual ICollection<User> Users{ get; set; }
+
+    }
+}
